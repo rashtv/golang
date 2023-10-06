6cb3b1d3bf8a1cd1799f2aaa4144e28d5dcbf9e6
Rashitov Ilyas <rashtv.19@gmail.com>
2023-10-07 00:50:03 +0600

    (custom) json decoding and bad request managing added

--- goProject/cmd/api/coins.go
+++ goProject/cmd/api/coins.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"fmt"
+	"goProject/internal/data"
 	"net/http"
+	"time"
 )
+
 func (app *application) createCoinHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new coin")
+	var input struct {
+		Title        string            `json:"title"`
+		Year         data.Year         `json:"year"`
+		Country      string            `json:"country"`
+		Status       string            `json:"status"`
+		Quantity     int64             `json:"quantity"`
+		Material     string            `json:"material"`
+		AuctionValue data.AuctionValue `json:"auction_value"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showCoinHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
+	if err != nil {
+		app.notFoundResponse(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of coin %d\n", id)
+	coin := data.Coin{
+		ID:           id,
+		CreatedAt:    time.Now(),
+		Title:        "100 KZT",
+		Country:      "Kazakhstan",
+		Status:       "Still in Production",
+		Quantity:     100000,
+		Material:     "Base metal alloys",
+		AuctionValue: 1000,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"coin": coin}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		app.serverErrorResponse(w, r, err)
+	}
 }
--- goProject/cmd/api/routes.go
+++ goProject/cmd/api/routes.go
@@ -7,8 +7,13 @@ import (
 
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/coins", app.createCoinHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/coins/:id", app.showCoinHandler)
+
 	return router
 }
