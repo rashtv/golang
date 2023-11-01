d0f194693c26821a10ee2207621db107ff39f776
Ilyas Rashitov <rashtv.19@gmail.com>
2023-11-01 18:37:06 +0600

    graceful shutdown added

--- numismatics_project/cmd/api/main.go
+++ numismatics_project/cmd/api/main.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"goProject/internal/data"
 	"goProject/internal/jsonlog"
-	"net/http"
 	"os"
 	"time"
 
@@ -73,21 +71,10 @@ func main() {
 		models: data.NewModel(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
