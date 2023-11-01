92af6a23b374e631adc6b1df40b2fc9f5f7b8164
Ilyas Rashitov <rashtv.19@gmail.com>
2023-11-01 17:59:16 +0600

    rate limiting added

--- numismatics_project/cmd/api/routes.go
+++ numismatics_project/cmd/api/routes.go
@@ -12,11 +12,12 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
 	router.HandlerFunc(http.MethodGet, "/v1/coins", app.listCoinsHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/coins", app.createCoinHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/coins/:id", app.showCoinHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/coins/:id", app.updateCoinHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/coins/:id", app.deleteCoinHandler)
 
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
