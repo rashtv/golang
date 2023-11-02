7243b093df444c635750a7f4cd2f790f548b1474
Ilyas Rashitov <rashtv.19@gmail.com>
2023-11-02 14:02:01 +0600

    creating and sending emails added

--- numismatics_project/cmd/api/main.go
+++ numismatics_project/cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"goProject/internal/data"
 	"goProject/internal/jsonlog"
+	"goProject/internal/mailer"
 	"os"
 	"time"
 
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -52,6 +61,13 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "26677a940315bc", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "5a5835ccf81a6a", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "numismatics@example.com",
+		"SMTP sender")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -69,6 +85,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModel(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
